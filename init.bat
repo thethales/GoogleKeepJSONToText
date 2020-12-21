31fc7509ef6ef14d0bb38d1579525e141abaf2b0
the.thales <thvms.thales@gmail.com>
2020-12-21 20:34:21 -0300

    Created file

---
+++ init.bat
@@ -0,0 +1,4 @@
+rem Arquivo de Inicialização de Dependencias de Desenvolvimento para Windows
+rem Incluir aqui todas as chamadas de ferramentas necessárias para inicialização do desenvolvimento
+code "%~dp0"
+cmd.exe
